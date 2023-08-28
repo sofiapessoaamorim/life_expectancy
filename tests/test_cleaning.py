c85ab1c70c98e84334c9198ff63d51557ecfdeb1
sofiapessoaamorim <sofia_amorim10@hotmail.com>
2023-08-28 17:18:15 +0100

    CLEAN : data refactoring

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,13 +1,13 @@
 """Tests for the cleaning module"""
 import pandas as pd
 
-from ..cleaning import clean_data
+from ..cleaning import main
 from . import OUTPUT_DIR
 
 
 def test_clean_data(pt_life_expectancy_expected):
     """Run the `clean_data` function and compare the output to the expected output"""
-    clean_data(region="PT")
+    main(region="PT")
     pt_life_expectancy_actual = pd.read_csv(
         OUTPUT_DIR / "pt_life_expectancy.csv"
     )
