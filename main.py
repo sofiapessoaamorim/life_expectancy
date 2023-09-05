82730eedde3d528aa4017cff8362f38fdf15a776
sofiapessoaamorim <sofia_amorim10@hotmail.com>
2023-09-05 13:03:32 +0100

    IMPROVE: folder structure improved

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 """Script to load, clean and save life expectancy data
 """
 import argparse
-from pandas import DataFrame
-from .data import load_data
+import pandas as pd
 from .cleaning import clean_data
 
 
@@ -12,28 +11,19 @@ def load_clean_save_data(region: str) -> None:
     Args:
         region: region to filter
     """
-    df_raw = load_data()
-    df_clean = clean_data(region, df_raw)
-    save_data(df_clean)
+    df_clean = clean_data(region)
+    save_file_to_csv(df_clean, region)
 
 
-def save_data(df_clean: DataFrame)-> None:
-    """Function to save life expectancy data to csv file
-
-    Args:
-        df: dataframe to save
-    """
-    return save_file_to_csv(df_clean)
-
-
-def save_file_to_csv(df_melt: DataFrame)-> None:
+def save_file_to_csv(df_melt: pd.DataFrame, region: str)-> None:
     """ Save melted and clean dataframe to csv file
 
     Args:
         df_melt: dataframe to save
+        region: data region
     """
     df_melt.to_csv(
-        "data/pt_life_expectancy.csv", 
+        f"data/{region.str.lower()}_life_expectancy.csv", 
         sep=",",
         index=False
     )
@@ -43,4 +33,4 @@ if __name__ == "__main__": #pragma: no cover
     parser = argparse.ArgumentParser(description='Example argparse')
     parser.add_argument("--region", "--r", type=str, default="PT", help='Region name')
     args = parser.parse_args()
-    main(args.region)
+    load_clean_save_data(args.region)
